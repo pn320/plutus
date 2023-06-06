40709bfed16f4a84ab44f0ff8cb3992b25cad184
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2023-06-06 18:15:01 +0100

    changed delay for second line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="w-full mx-auto max-w-hero mt-40 isolate px-14">
       <div className="relative text-center flex flex-col items-center before:absolute before:h-[1px] before:bg-gridline before:bg-[length:5px_1px] before:grid-fade before:animate-grid before:delay-[0.15s] before:top-0 before:left-[-75px]">
-        <h1 className="relative font-sans p-6 lg:text-hero md:text-6xl leading-hero bg-hero text-transparent bg-clip-text font-extrabold tracking-[-0.035em] after:absolute after:h-[1px] after:bg-gridline after:bg-[length:5px_1px] after:grid-fade after:animate-grid after:delay-[0.16s] after:bottom-[-0.1px] after:left-[-75px]">
+        <h1 className="relative font-sans p-6 lg:text-hero md:text-6xl leading-hero bg-hero text-transparent bg-clip-text font-extrabold tracking-[-0.035em] after:absolute after:h-[1px] after:bg-gridline after:bg-[length:5px_1px] after:grid-fade after:animate-grid after:delay-[0.25s] after:bottom-[-0.1px] after:left-[-75px]">
           The Finance Toolkit for Everyone
         </h1>
         <div>
